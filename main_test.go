90bc0dfde64e04c8dd5ae79a277452954258be16
Elena Samsonova <samsonova.elena@gmail.com>
2024-07-05 11:35:59 -0700

    Add unit test for ProcessFile func

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,4 +16,21 @@ func TestMin_AllPositiveNumbers(t *testing.T) {
 func TestMin_OneNegativeNumber(t *testing.T) {
 	r := min(-14, 7)
 	assert.Equal(t, -14, r, `Expected -14 as minumum of -14 and 7`)
+}
+
+func TestProcessFile(t *testing.T) {
+	ProcessFile("a.log")
+	assert.True(t, EqualFiles("a_answer.xcbr", "a.xcbr"))
+}
+
+func EqualFiles(f1, f2 string) bool {
+	f1Content, err := os.ReadFile(f1)
+	if err != nil {
+		return false
+	}
+	f2Content, err := os.ReadFile(f2)
+	if err != nil {
+		return false
+	}
+	return bytes.Equal(f1Content, f2Content)
 }
