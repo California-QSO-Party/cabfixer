2572ba9d89e2179fc692e67ffc0845e4bf7870f7
Elena Samsonova <samsonova.elena@gmail.com>
2024-07-28 18:58:00 -0700

    Loop over all available tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+for i in *.log; do
+  ./cabfixer $i
+  filename=$(basename $i .log)
+  output_file=$filename.xcbr
+  echo $output_file
+  answer_file=${filename}_answer.xcbr
+  echo $answer_file
+  dos2unix $answer_file
+  diff $output_file $answer_file
+  if  [[ $? -eq 0 ]]; then
+    echo "test passed"
+  else
+    echo "test failed"
+  fi
 
-./cabfixer a.log
-diff a.xcbr a_answer.xcbr
-if  [[ $? -eq 0 ]]; then
-  echo "test passed"
-else
-  echo "test failed"
-fi
+done
